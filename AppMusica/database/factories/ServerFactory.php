1863d9ca0205c5208eb9fce15fdd5e36645083d6
David Valero Croma <david.valero@usach.cl>
2022-06-13 02:39:41 -0400

    [David Valero]
    Se crearon los factories de server y song server

---
+++ AppMusica/database/factories/ServerFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Server>
+ */
+class ServerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'name_server' => $this->faker->name,
+            'ubicacion'  => $this->faker->ipv4
+        ];
+    }
+}
---
+++ AppMusica/database/factories/Song_serverFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Song_server>
+ */
+class Song_serverFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'id_song' => Song::all() -> random() -> id_song,
+            'id_server' => Server::all() -> random() -> id_server
+        ];
+    }
+}

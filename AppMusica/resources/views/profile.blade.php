f0594c163109710c3125d09df5f801750613f816
Agustin Haristoy <agustin.haristoy@usach.cl>
2022-06-28 01:21:11 -0400

    [Agustin Haristoy]
    Links de prueba en perfil

--- AppMusica/resources/views/profile.blade.php
+++ AppMusica/resources/views/profile.blade.php
@@ -8,7 +8,7 @@
 <div class="card" style="max-width: 540px;">
     <div class="row g-0">
         <div class="col-md-4">
-            <a href="https://ibb.co/TmkwfHm"><img src="https://i.ibb.co/TmkwfHm/perritodelbar.jpg"
+            <a href="https://www.instagram.com/perritodelbar/"><img src="https://i.ibb.co/TmkwfHm/perritodelbar.jpg"
                     alt="perritodelbar"></a>
         </div>
         <div class="col-md-8">
@@ -33,7 +33,7 @@
                             <div class="row">
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/D159bPC"><img
+                                        <a href="https://www.youtube.com/watch?v=vS46sgmnPQE"><img
                                                 src="https://i.ibb.co/D159bPC/reggaeton.jpg" alt="reggaeton"></a>
                                         <div class="card-body">
                                             <p class="card-text">Mix Reggaeton</p>
@@ -43,7 +43,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/BNYvWcX"><img
+                                        <a href="https://www.youtube.com/watch?v=2WLyFO4ZFfU"><img
                                                 src="https://i.ibb.co/BNYvWcX/Clasic-Rock.jpg" alt="Clasic-Rock"></a>
                                         <div class="card-body">
                                             <p class="card-text">Clasic Rock</p>
@@ -53,7 +53,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/8YPSxDJ"><img
+                                        <a href="https://www.youtube.com/watch?v=M8GYYAcdgb0"><img
                                                 src="https://i.ibb.co/8YPSxDJ/Pop-Music.jpg" alt="Pop-Music"></a>
                                         <div class="card-body">
                                             <p class="card-text">Best Pop Music</p>
@@ -63,7 +63,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/0JzyrGZ"><img
+                                        <a href="https://www.youtube.com/watch?v=gQlUknjUX0I"><img
                                                 src="https://i.ibb.co/0JzyrGZ/Electro-House.jpg"
                                                 alt="Electro-House"></a>
                                         <div class="card-body">
@@ -81,7 +81,7 @@
                             <div class="row">
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/wSKqLpF"><img src="https://i.ibb.co/wSKqLpF/fiesta-tropical.jpg" alt="fiesta-tropical"></a>
+                                        <a href="https://www.youtube.com/watch?v=Vzy8gCdfdkE"><img src="https://i.ibb.co/wSKqLpF/fiesta-tropical.jpg" alt="fiesta-tropical"></a>
                                         <div class="card-body">
                                             <p class="card-text">Fiesta Tropical</p>
                                             <p class="card-text"><small class="text-muted">Various Artists</small></p>
@@ -90,7 +90,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/tq14hTg"><img src="https://i.ibb.co/tq14hTg/reggae.jpg" alt="reggae"></a>
+                                        <a href="https://www.youtube.com/watch?v=EkUNRrSft-g"><img src="https://i.ibb.co/tq14hTg/reggae.jpg" alt="reggae"></a>
                                         <div class="card-body">
                                             <p class="card-text">Reggae Music</p>
                                             <p class="card-text"><small class="text-muted">Various Artists</small></p>
@@ -99,7 +99,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/sHvT84C"><img src="https://i.ibb.co/sHvT84C/cumbia-chilena.jpg" alt="cumbia-chilena"></a>
+                                        <a href="https://www.youtube.com/watch?v=LioiF4XdRwQ"><img src="https://i.ibb.co/sHvT84C/cumbia-chilena.jpg" alt="cumbia-chilena"></a>
                                         <div class="card-body">
                                             <p class="card-text">Cumbia Chilena</p>
                                             <p class="card-text"><small class="text-muted">Various Artists</small></p>
@@ -108,7 +108,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/DRS75zm"><img src="https://i.ibb.co/DRS75zm/trap.jpg" alt="trap"></a>
+                                        <a href="https://www.youtube.com/watch?v=Cn5N2_dYfFY"><img src="https://i.ibb.co/DRS75zm/trap.jpg" alt="trap"></a>
                                         <div class="card-body">
                                             <p class="card-text">Trap Music</p>
                                             <p class="card-text"><small class="text-muted">Various Artists</small></p>
@@ -144,7 +144,7 @@
                             <div class="row">
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/YX90THB"><img src="https://i.ibb.co/YX90THB/woof.jpg" alt="woof"></a>
+                                        <a href="https://www.youtube.com/watch?v=mE7lTMxlJI0"><img src="https://i.ibb.co/YX90THB/woof.jpg" alt="woof"></a>
                                         <div class="card-body">
                                             <p class="card-text">Woof!</p>
                                             <p class="card-text"><small class="text-muted">Snoop Dogg</small></p>
@@ -153,7 +153,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/H28XGZ3"><img src="https://i.ibb.co/H28XGZ3/guach-perry.jpg" alt="guach-perry"></a>
+                                        <a href="https://www.youtube.com/watch?v=H7dQCq1lQtM"><img src="https://i.ibb.co/H28XGZ3/guach-perry.jpg" alt="guach-perry"></a>
                                         <div class="card-body">
                                             <p class="card-text">Guach Perry</p>
                                             <p class="card-text"><small class="text-muted">Chancho En Piedra</small></p>
@@ -162,7 +162,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/zXcVfxs"><img src="https://i.ibb.co/zXcVfxs/Perro-Chocolo.jpg" alt="Perro-Chocolo"></a>
+                                        <a href="https://www.youtube.com/watch?v=y-Hij3yMD60"><img src="https://i.ibb.co/zXcVfxs/Perro-Chocolo.jpg" alt="Perro-Chocolo"></a>
                                         <div class="card-body">
                                             <p class="card-text">Así yo ladro</p>
                                             <p class="card-text"><small class="text-muted">El Perro Chocolo</small></p>
@@ -171,7 +171,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/c3rBjpk"><img src="https://i.ibb.co/c3rBjpk/Doggy-Style.webp" alt="Doggy-Style"></a>
+                                        <a href="https://www.youtube.com/watch?v=z1CSneHgd6c"><img src="https://i.ibb.co/c3rBjpk/Doggy-Style.webp" alt="Doggy-Style"></a>
                                         <div class="card-body">
                                             <p class="card-text">Doggy Style</p>
                                             <p class="card-text"><small class="text-muted">31 Minutos</small></p>
@@ -187,7 +187,7 @@
                             <div class="row">
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/f8dQZ52"><img src="https://i.ibb.co/f8dQZ52/perros-salvajes.jpg" alt="perros-salvajes"></a>
+                                        <a href="https://www.youtube.com/watch?v=UaXqj5CBtMU"><img src="https://i.ibb.co/f8dQZ52/perros-salvajes.jpg" alt="perros-salvajes"></a>
                                         <div class="card-body">
                                             <p class="card-text">Perros Salvajes</p>
                                             <p class="card-text"><small class="text-muted">Daddy Yankee</small></p>
@@ -196,7 +196,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/txcVyDb"><img src="https://i.ibb.co/txcVyDb/Exilio.jpg" alt="Exilio"></a>
+                                        <a href="https://www.youtube.com/watch?v=jM3gkvfg2WA"><img src="https://i.ibb.co/txcVyDb/Exilio.jpg" alt="Exilio"></a>
                                         <div class="card-body">
                                             <p class="card-text">Exilio</p>
                                             <p class="card-text"><small class="text-muted">Perrosky</small></p>
@@ -205,7 +205,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/4M75qdd"><img src="https://i.ibb.co/4M75qdd/Baile-del-Perro.jpg" alt="Baile-del-Perro"></a>
+                                        <a href="https://www.youtube.com/watch?v=hxjf_Wnc0SU"><img src="https://i.ibb.co/4M75qdd/Baile-del-Perro.jpg" alt="Baile-del-Perro"></a>
                                         <div class="card-body">
                                             <p class="card-text">El Baile del Perrito</p>
                                             <p class="card-text"><small class="text-muted">Wilfrido Vargas</small></p>
@@ -214,7 +214,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/Hh3RWpN"><img src="https://i.ibb.co/Hh3RWpN/perrito-malvado.jpg" alt="perrito-malvado"></a>
+                                        <a href="https://www.youtube.com/watch?v=JasYNAEgehM"><img src="https://i.ibb.co/Hh3RWpN/perrito-malvado.jpg" alt="perrito-malvado"></a>
                                         <div class="card-body">
                                             <p class="card-text">Perrito Malvado</p>
                                             <p class="card-text"><small class="text-muted">Damas Gratis</small></p>

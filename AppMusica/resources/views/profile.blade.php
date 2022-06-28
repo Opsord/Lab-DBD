321c9b7284c749e6863ec065043b04728d475339
Agustin Haristoy <agustin.haristoy@usach.cl>
2022-06-27 21:26:35 -0400

    [Agustin Haristoy]
    Se agrega el carrusel a la vista perfil

--- AppMusica/resources/views/profile.blade.php
+++ AppMusica/resources/views/profile.blade.php
@@ -8,7 +8,8 @@
 <div class="card" style="max-width: 540px;">
     <div class="row g-0">
         <div class="col-md-4">
-            <a href="https://ibb.co/MGnWpGg"><img src="https://i.ibb.co/MGnWpGg/perritodelbar.jpg" alt="perritodelbar"></a>
+            <a href="https://ibb.co/TmkwfHm"><img src="https://i.ibb.co/TmkwfHm/perritodelbar.jpg"
+                    alt="perritodelbar"></a>
         </div>
         <div class="col-md-8">
             <div class="card-body">
@@ -22,45 +23,220 @@
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="text-light">Playlists</h1>
 </div>
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-      <div class="col">
-        <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-            <a href="https://ibb.co/D159bPC"><img src="https://i.ibb.co/D159bPC/reggaeton.jpg" alt="reggaeton"></a>
-            <div class="card-body">
-              <p class="card-text">Mix Reggaeton</p>
-              <p class="card-text"><small class="text-muted">Various Artists</small></p>
+        <div class="col">
+            <div id="inam" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/D159bPC"><img
+                                                src="https://i.ibb.co/D159bPC/reggaeton.jpg" alt="reggaeton"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Mix Reggaeton</p>
+                                            <p class="card-text"><small class="text-muted">Various Artists</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/BNYvWcX"><img
+                                                src="https://i.ibb.co/BNYvWcX/Clasic-Rock.jpg" alt="Clasic-Rock"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Clasic Rock</p>
+                                            <p class="card-text"><small class="text-muted">Various Artists</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/8YPSxDJ"><img
+                                                src="https://i.ibb.co/8YPSxDJ/Pop-Music.jpg" alt="Pop-Music"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Best Pop Music</p>
+                                            <p class="card-text"><small class="text-muted">Various Artists</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/0JzyrGZ"><img
+                                                src="https://i.ibb.co/0JzyrGZ/Electro-House.jpg"
+                                                alt="Electro-House"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Electro House</p>
+                                            <p class="card-text"><small class="text-muted">Various Artists</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item active">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/wSKqLpF"><img src="https://i.ibb.co/wSKqLpF/fiesta-tropical.jpg" alt="fiesta-tropical"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Fiesta Tropical</p>
+                                            <p class="card-text"><small class="text-muted">Various Artists</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/tq14hTg"><img src="https://i.ibb.co/tq14hTg/reggae.jpg" alt="reggae"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Reggae Music</p>
+                                            <p class="card-text"><small class="text-muted">Various Artists</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/sHvT84C"><img src="https://i.ibb.co/sHvT84C/cumbia-chilena.jpg" alt="cumbia-chilena"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Cumbia Chilena</p>
+                                            <p class="card-text"><small class="text-muted">Various Artists</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/DRS75zm"><img src="https://i.ibb.co/DRS75zm/trap.jpg" alt="trap"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Trap Music</p>
+                                            <p class="card-text"><small class="text-muted">Various Artists</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#inam" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#inam" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
-          </div>
-      </div>
-      <div class="col">
-        <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-            <a href="https://ibb.co/BNYvWcX"><img src="https://i.ibb.co/BNYvWcX/Clasic-Rock.jpg" alt="Clasic-Rock"></a>
-            <div class="card-body">
-              <p class="card-text">Clasic Rock</p>
-              <p class="card-text"><small class="text-muted">Various Artists</small></p>
-            </div>
-          </div>
-      </div>
-      <div class="col">
-        <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-            <a href="https://ibb.co/8YPSxDJ"><img src="https://i.ibb.co/8YPSxDJ/Pop-Music.jpg" alt="Pop-Music"></a>
-            <div class="card-body">
-              <p class="card-text">Best Pop Music</p>
-              <p class="card-text"><small class="text-muted">Various Artists</small></p>
-            </div>
-          </div>
-      </div>
-      <div class="col">
-        <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-            <a href="https://ibb.co/0JzyrGZ"><img src="https://i.ibb.co/0JzyrGZ/Electro-House.jpg" alt="Electro-House"></a>
-            <div class="card-body">
-              <p class="card-text">Electro House</p>
-              <p class="card-text"><small class="text-muted">Various Artists</small></p>
-            </div>
-          </div>
-      </div>
+        </div>
     </div>
-  </div>
+</div>
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="text-light">Canciones Favoritas</h1>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <div id="inam2" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/YX90THB"><img src="https://i.ibb.co/YX90THB/woof.jpg" alt="woof"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Woof!</p>
+                                            <p class="card-text"><small class="text-muted">Snoop Dogg</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/H28XGZ3"><img src="https://i.ibb.co/H28XGZ3/guach-perry.jpg" alt="guach-perry"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Guach Perry</p>
+                                            <p class="card-text"><small class="text-muted">Chancho En Piedra</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/zXcVfxs"><img src="https://i.ibb.co/zXcVfxs/Perro-Chocolo.jpg" alt="Perro-Chocolo"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Así yo ladro</p>
+                                            <p class="card-text"><small class="text-muted">El Perro Chocolo</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/c3rBjpk"><img src="https://i.ibb.co/c3rBjpk/Doggy-Style.webp" alt="Doggy-Style"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Doggy Style</p>
+                                            <p class="card-text"><small class="text-muted">31 Minutos</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item active">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/f8dQZ52"><img src="https://i.ibb.co/f8dQZ52/perros-salvajes.jpg" alt="perros-salvajes"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Perros Salvajes</p>
+                                            <p class="card-text"><small class="text-muted">Daddy Yankee</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/txcVyDb"><img src="https://i.ibb.co/txcVyDb/Exilio.jpg" alt="Exilio"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Exilio</p>
+                                            <p class="card-text"><small class="text-muted">Perrosky</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/4M75qdd"><img src="https://i.ibb.co/4M75qdd/Baile-del-Perro.jpg" alt="Baile-del-Perro"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">El Baile del Perrito</p>
+                                            <p class="card-text"><small class="text-muted">Wilfrido Vargas</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/Hh3RWpN"><img src="https://i.ibb.co/Hh3RWpN/perrito-malvado.jpg" alt="perrito-malvado"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">Perrito Malvado</p>
+                                            <p class="card-text"><small class="text-muted">Damas Gratis</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#inam2" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#inam2" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 @endsection

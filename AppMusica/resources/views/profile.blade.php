5751bdec4fa194e659ddab3325b5ced2d055c0f0
Agustin Haristoy <agustin.haristoy@usach.cl>
2022-07-08 04:16:34 -0400

    [Agustin Haristoy]
    Pruebas en profile 4

--- AppMusica/resources/views/profile.blade.php
+++ AppMusica/resources/views/profile.blade.php
@@ -13,7 +13,7 @@
         <div class="col-md-8">
             <div class="card-body">
                 <h5 class="card-title">{{$user->name_user}}</h5>
-                <p class="card-text">Intente ligar con una chica informatica pero no se deJava</p>
+                <p class="card-text">Intente ligar con una informatica pero no se deJava</p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
         </div>
@@ -30,71 +30,57 @@
                     <div class="carousel-item active">
                         <div class="container">
                             <div class="row">
-                                @php
-                                $contador1 = 0;
-                                @endphp
-                                @foreach($playlists as $p) <div class="col">
-                                    @foreach($users_playlist as $u_p)
-                                    @if($u_p->id_playlist == $p->id_playlist && $u_p->id_user == $user->id_user &&
-                                    $contador
-                                    < 4) <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="https://ibb.co/K96Nm7K"><img
-                                                src="https://i.ibb.co/K96Nm7K/music-album-1.png" alt="music-album-1"
-                                                borde="0"></a>
+                                @for($i = 0;$i < 4;$i++)
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/K96Nm7K" ><img src="https://i.ibb.co/K96Nm7K/music-album-1.png" alt="music-album-1" borde="0"></a>
                                         <div class="card-body">
-                                            <p class="card-text">{{$p->name_playlist}}</p>
-                                            <p class="card-text"><small class="text-muted">{{$user->name_user}}</small>
-                                            </p>
+                                            <p class="card-text">{{$playlists[$i]->name_playlist}}</p>
+                                            @foreach($users_playlist as $u_p)
+                                            @if($playlists[$i]->id_playlist == $u_p->id_playlist)
+                                            <p class="card-text"><small class="text-muted">{{$artist[$u_p->id_user -1]->name_user}}</small></p>
                                         </div>
-                                </div>
-                                @endif
-                                @endforeach
-                            </div>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item active">
-                    <div class="container">
-                        <div class="row">
-                            @php
-                            $contador = 0;
-                            @endphp
-                            @foreach($playlists as $p) <div class="col">
-                                @foreach($users_playlist as $u_p)
-                                @if($u_p->id_playlist == $p->id_playlist && $u_p->id_user == $user->id_user && $contador
-                                < 4) <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                    <a href="https://ibb.co/K96Nm7K"><img
-                                            src="https://i.ibb.co/K96Nm7K/music-album-1.png" alt="music-album-1"
-                                            borde="0"></a>
-                                    <div class="card-body">
-                                        <p class="card-text">{{$p->name_playlist}}</p>
-
-                                        <p class="card-text"><small class="text-muted">{{$user->name_user}}</small></p>
-                                        @php
-                                        $contador = contador + 1;
-                                        @endphp
+                                        @endif
+                                        @endforeach
                                     </div>
+                                </div>
+                                @endfor
                             </div>
-                            @endif
-                            @endforeach
                         </div>
-                        @endforeach
+                    </div>
+                    <div class="carousel-item active">
+                        <div class="container">
+                            <div class="row">
+                                @for($i = 4;$i < 8;$i++)
+                                <div class="col">
+                                    <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
+                                        <a href="https://ibb.co/K96Nm7K"><img src="https://i.ibb.co/K96Nm7K/music-album-1.png" alt="music-album-1" borde="0"></a>
+                                        <div class="card-body">
+                                            <p class="card-text">{{$playlists[$i]->name_playlist}}</p>
+                                            @foreach($users_playlist as $u_p)
+                                            @if($playlists[$i]->id_playlist == $u_p->id_playlist)
+                                            <p class="card-text"><small class="text-muted">{{$artist[$u_p->id_user -1]->name_user}}</small></p>
+                                        </div>
+                                        @endif
+                                        @endforeach
+                                    </div>
+                                </div>
+                                @endfor
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#inam" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#inam" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#inam" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#inam" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
     </div>
-</div>
-</div>
 </div>
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="text-light">Canciones Favoritas</h1>
@@ -107,53 +93,49 @@
                     <div class="carousel-item active">
                         <div class="container">
                             <div class="row">
-                                @for($i = 0;$i < 4;$i++) <div class="col">
+                                @for($i = 0;$i < 4;$i++)
+                                <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="{{url('songview')}}/{{$songs[$i]->id_song}}"><img
-                                                src="https://i.ibb.co/1rqXc67/zhiv-song-4.png" alt="zhiv-song-4"
-                                                borde="0"></a>
+                                        <a href="{{url('songview')}}/{{$songs[$i]->id_song}}"><img src="https://i.ibb.co/1rqXc67/zhiv-song-4.png" alt="zhiv-song-4" borde="0"></a>
                                         <div class="card-body">
                                             <p class="card-text">{{$songs[$i]->name_song}}</p>
-                                            <p class="card-text"><small class="text-muted">{{$artist[$songs[$i]->artist -
-                                                    1]->name_user}}</small></p>
+                                            <p class="card-text"><small class="text-muted">Snoop Dogg</small></p>
                                         </div>
                                     </div>
+                                </div>
+                                @endfor
                             </div>
-                            @endfor
                         </div>
                     </div>
                     <div class="carousel-item active">
                         <div class="container">
                             <div class="row">
-                                @for($i = 4;$i < 8;$i++) <div class="col">
+                                @for($i = 4;$i < 8;$i++)
+                                <div class="col">
                                     <div class="card text-bg-warning mb-3" style="width: 11.4rem;">
-                                        <a href="{{url('songview')}}/{{$songs[$i]->id_song}}"><img
-                                                src="https://i.ibb.co/1rqXc67/zhiv-song-4.png" alt="zhiv-song-4"
-                                                borde="0"></a>
+                                        <a href="{{url('songview')}}/{{$songs[$i]->id_song}}"><img src="https://i.ibb.co/1rqXc67/zhiv-song-4.png" alt="zhiv-song-4" borde="0"></a>
                                         <div class="card-body">
-                                            <p class="card-text">{{$songs[i]->name_song}}</p>
-                                            <p class="card-text"><small class="text-muted">{{$artist[$songs[$i]->artist -
-                                                    1]->name_user}}</small></p>
+                                            <p class="card-text">{{$songs[$i]->name_song}}</p>
+                                            <p class="card-text"><small class="text-muted">{{$artist[$songs[$i]->artist]->name_user}}</small></p>
                                         </div>
                                     </div>
+                                </div>
+                                @endfor
                             </div>
                         </div>
-                        @endfor
                     </div>
                 </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#inam2" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#inam2" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#inam2" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#inam2" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
     </div>
-</div>
-</div>
 </div>
 
 
